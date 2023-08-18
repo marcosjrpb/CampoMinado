7b21a11dfb1fd56dfa6426b118d74fa78a3f0f19
marcosjrpb <marcosjrpb@hotmail.com>
2023-08-17 22:47:33 -0300

    Class Tabuleiro part 1

--- src/br/com/game/cm/modelo/Tabuleiro.java
+++ src/br/com/game/cm/modelo/Tabuleiro.java
@@ -1,5 +1,52 @@
 package br.com.game.cm.modelo;
 
-public class Tabuleiro {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo>campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associaVizinhos();
+		sortearMinas();
+	}
+
+
+
+	private void gerarCampos() {
+		
+		for (int i = 0; i < linhas; i++) {
+			 for (int j = 0; j < colunas; j++) {
+				campos.add(new Campo(i,j));
+			}			
+		}		
+	}
+	
+	private void associaVizinhos() {	
+		
+		for(Campo c1: campos) {
+			for(Campo c2: campos) {
+				c1.adicionarVizinho(c2);
+			}
+			
+		}
+	}
+	
+	private void sortearMinas() {
+		
+	}
+
+	
+	
 }
